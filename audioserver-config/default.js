93f06293fdb18d48fd2a25dcc81cbd1c49cc92da
Rob Pickering <rob@pickering.org>
2020-05-12 00:20:04 +0100

    working config

--- audioserver-config/default.js
+++ audioserver-config/default.js
@@ -1,20 +1,46 @@
 module.exports = {
-    rtpServer: {
-        port: 7777,
-        host: 'audioserver',
-        swap16: true
-    },
-    mqtt: {
-        url: 'mqtt://test.mosquitto.org',
-        prefix: 'rjp44'
-    },
-    dialogflow: {
+  rtpServer: {
+    port: 7777,
+    host: '0.0.0.0',
+    swap16: true
+  },
+  mqtt: {
+    url: 'mqtt://test.mosquitto.org',
+    prefix: 'rjp44'
+  },
+  asterisk: {
+    // If set then Playback the Asterisk sound file specified before bridging to the dialogflow media.
+    // Useful for debug and breaking media stand-offs.
+    playback: 'silence/1'
+  },
+  dialogflow: {
+    auth: {
         //credentials: {
         //     private_key: response.dialogFlowPrivateKey,
         //     client_email: response.dialogFlowClientEmail
         //   }
         keyFilename: `./config/credentials.json`,
-        project: 'mutualassistclient-rgqcew',
-        initialEventName: 'WELCOME'
+    },
+    project: 'mutualassistclient-rgqcew',
+    initialEventName: 'WELCOME',
+    enableOutputSpeech: true,
+    audioInputConfig: {
+      audioEncoding: 'AUDIO_ENCODING_LINEAR_16',
+      sampleRateHertz: 16000,
+      languageCode: 'en',
+      singleUtterance: false
+    },
+    audioOutputConfig: {
+      audioEncoding: 'OUTPUT_AUDIO_ENCODING_LINEAR_16',
+      sampleRateHertz: 8000, // should be the same as dialogFlowAudioInputConfig.sampleRateHertz but Asterisk doesnt like 16000 back
+      synthesizeSpeechConfig: {
+          speakingRate: 1,
+          pitch: 5,
+          volumeGainDb: 0,
+          voice: {
+              ssmlGender: `SSML_VOICE_GENDER_FEMALE`
+          }
+      }
     }
+  }
 }
--- bridge-config/default.js
+++ bridge-config/default.js
@@ -1,17 +1,22 @@
 module.exports = {
-    ari: {
-        url: 'http://asterisk:8080',
-        username: 'dialogflow',
-        password: 'dialogflow',
-        appName: 'dialogflow'
-    },
-    rtpServer: {
-        host: 'audioserver,
-        port: '7777',
-        format: 'slin16'
-    },
-    mqtt: {
-        url: 'mqtt://test.mosquitto.org',
-        topicPrefix: 'rjp44'
-    }
+  ari: {
+    url: 'http://asterisk:8080',
+    username: 'dialogflow',
+    password: 'dialogflow',
+    appName: 'dialogflow'
+  },
+  rtpServer: {
+    host: 'audioserver',
+    port: 7777,
+    format: 'slin16'
+  },
+  mqtt: {
+    url: 'mqtt://test.mosquitto.org',
+    topicPrefix: 'rjp44'
+  },
+  asterisk: {
+    // If set then Playback the Asterisk sound file specified before bridging to the dialogflow media.
+    // Useful for debug and breaking media stand-offs.
+    playback: 'silence/1'
+  },
 }
